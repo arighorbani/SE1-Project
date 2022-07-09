6e7367d436ef0a8b9e68f6a81da00465c7a13d58
Arian Ghorbani <arighorbani@gmail.com>
2022-07-09 08:06:23 -0700

    setup for function creation

--- basefunct.py
+++ basefunct.py
@@ -1,4 +1,4 @@
-def newClass():
+def newClass(): #function that takes new class input and makes a new object out of it
     coursename = input("Please enter the name of the course: ")
     term_str = input("Please enter the term in which you took the course: ")
     term = term_str.split(" ")
@@ -8,6 +8,13 @@ def newClass():
     profrating = int(input("Please enter how you would rate the professor from 1 to 10: ")) #TODO: errorcheck
     currclass = [coursename, term, grade, profname, courserating, profrating]
     return currclass
+
+def printClasses(listoflists): #TODO: make the printing of classes into a function
+    pass
+
+def sortClasses(listoflists): #TODO: make sort into a function
+    pass
+
 
 loop = True #set to false when program is ending
 print("Welcome to the class diagnostic program.\n\n")
@@ -22,13 +29,13 @@ while loop == True:
             #TODO: loop on the display of classes
             whichclass = 1
             for x in classlist:
-                print(str(whichclass) + ".       Course name: "  + x[0] + '\n')
+                print("\n" + str(whichclass) + ".       Course name: "  + x[0])
                 whichclass += 1
-                print("         Term: " + x[1][0] + " " + x[1][1] + '\n') #TODO: make this info sortable!
-                print("         Grade: " + str(x[2]) + '\n')
-                print("         Professor's Name: " + x[3] + '\n')
+                print("         Term: " + x[1][0] + " " + x[1][1]) #TODO: make this info sortable!
+                print("         Grade: " + str(x[2]))
+                print("         Professor's Name: " + x[3])
                 print("         Course Rating: " + str(x[4]))
-                print("         Professor's Rating: " + str(x[5]) + "\n\n\n")
+                print("         Professor's Rating: " + str(x[5]) + "\n")
             #TODO: translate the percentage-based grade  the user entered into a letter grade
             
             print("a. Sort Classes by Term (NOT CURRENTLY WORKING)") #TODO: implement sort
