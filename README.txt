472b8a3a7d41063c71c8c26689c24c79b80c3813
Arian Ghorbani <arighorbani@gmail.com>
2022-07-25 18:31:26 -0700

    Example of microservice

---
+++ README.txt
@@ -0,0 +1,4 @@
+HOW TO REQUEST DATA: A javascript file will be set up requiring Seneca to work as the broker. Via node.js, Seneca can be requested to broker a service.
+HOW TO RECEIVE DATA: Seneca will provide a listen on a port. When a call is made on that port, Seneca will send the requested data.
+
+UML DIAGRAM: https://lucid.app/lucidchart/5bf5fa2d-e219-4cb3-9c3b-7de51fd7da99/edit?viewport_loc=-218%2C-21%2C3076%2C1497%2C0_0&invitationId=inv_b1aebb8e-d860-410a-96a5-98c87fcb7c79#
--- microservice/gtrends_scraper.py
+++ microservice/gtrends_scraper.py
@@ -1,41 +1,32 @@
 from pytrends.request import TrendReq
 pytrends = TrendReq(hl='en-US')
 
-all_keywords = [
 
+#SET OF VARIABLES THAT WILL NEED TO BE SENT IN THE SENECA REQUEST; have been set in order to demonstrate microservice as functional
+all_keywords = [
     'event management',
 
     'event planning',
 
     'event planner'
-
     ]
 
 cat = '14' #people&society
 
-timeframes = [
-
-    'today 5-y',
-
-    'today 12-m',
-
-    'today 3-m',
-
-    'today 1-m'
-
-    ]
+timeframe = 'today 5-y'
 
 geo = '' #worldwide
 
 gprop = '' #websearch
 
+#places into "pytrends" all of the data scraped
 pytrends.build_payload(
 
     all_keywords,
 
     cat,
 
-    timeframes[0],
+    timeframe,
 
     geo,
 
@@ -43,30 +34,4 @@ pytrends.build_payload(
 
     )
 
-
-keywords = []
-def check_trends():
-
-    pytrends.build_payload(
-
-        keywords,
-
-        cat,
-
-        timeframes[0],
-
-        geo,
-
-        gprop
-
-        )
-
-    data = pytrends.interest_over_time()
-
-for keyword in all_keywords:
-
-    keywords.append(keyword)
-
-    check_trends()
-
-    keywords.pop()
+print(pytrends.interest_over_time().to_string()) #prints "interest over time" data; EXAMPLE OF WHAT KIND OF DATA WILL BE RECEIVED
